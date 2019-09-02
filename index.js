4bce2e48a05f908f1a4d82c3bfff3e541fce9de1
FellRamos <x.fellr@gmail.com>
2019-09-02 10:58:58 +0200

    Add validation of blocks
    Added validation of blocks. First we validate the block itself,
    comparing its own hash value with a new hash calculated in that moment.
    Second, we check the current Block Previous Hash agains the Previous
    Block Hash. If everything is OK, we return true.
    
    Also added in the constructor this.hash = this.calculateHash(). No need
    to create the Genesis Block with this line now, is automatically added
    also to the Genesis Block

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class Block {
     this.timestamp = timestamp
     this.data = data
     this.previousHash = previousHash
+    this.hash = this.calculateHash()
   }
 
   calculateHash() {
@@ -21,8 +22,8 @@ class Blockchain {
 
     createGenesisBlock() {
       let genesisBlock = new Block(0, "01/09/2019", "Genesis Block", "0")
-      genesisBlock.hash = genesisBlock.calculateHash()
-      return genesisBlock
+      // genesisBlock.hash = genesisBlock.calculateHash() // Nao preciso agora --> Estou a dizer no constructor
+      return genesisBlock // que this.hash = this.calculateHash(), por isso e feito automaticamente na criacao do bloco.
     }
 
     getLatestBlock() {
@@ -35,6 +36,26 @@ class Blockchain {
       this.chain.push(newBlock)
     }
 
+    isChainValid() {
+      for (let i = 1; i < this.chain.length; i++) { // o i pode comecar pelo 1, que compara com o 0 (genesis block)
+        const currentBlock = this.chain[i]
+        const previousBlock = this.chain[i-1]
+        
+        // validate data integrity - Fazemos re-hash do proprio bloco e comparamos com o valor ja guardado
+        if (currentBlock.hash !== currentBlock.calculateHash()) {
+          return false
+        }
+
+        // Validate hash chain link - Verificamos o valor de previous hash do bloco com o hash do anterior!
+        if (currentBlock.previousHash !== previousBlock.hash) {
+          return false
+        }
+      }
+
+      // all is good
+      return true
+    }
+
 }
 
 let demoChain = new Blockchain()
@@ -43,3 +64,4 @@ demoChain.addBlock( new Block(1, "02/09/2019", {amount: 10} ) )
 demoChain.addBlock( new Block(2, "03/09/2019", {amount: 25} ) )
 
 console.log(JSON.stringify(demoChain, null, 4))
+console.log(demoChain.isChainValid())
